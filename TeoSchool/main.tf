3919d61fa0fce0a22b738ffc1062a1e8320cfdaa
sallby <djiby-oumar.sall@teolia.fr>
2024-03-05 13:23:33 +0100

    add role service principal

--- TeoSchool/main.tf
+++ TeoSchool/main.tf
@@ -49,13 +49,22 @@ resource "aws_iam_role" "eks_node_role" {
   name = "eks_node_role"
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
-    "Statement" : [{
-      "Effect" : "Allow",
-      "Principal" : {
-        "Service" : "ec2.amazonaws.com"
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
       },
-      "Action" : "sts:AssumeRole"
-    }]
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "eks.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
   })
 }
 
